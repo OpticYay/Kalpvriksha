16968347b797b4b888f1c80bc42b905d42cf1659
Lakshya Garg <garg.lakshya03@gmail.com>
2024-12-20 19:48:33 +0530

    updated code

--- Calculator.c
+++ Calculator.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
-int values[100];
-char operators[100];
+#define MAX 100
+
+int values[MAX];
+char operators[MAX];
 int top = -1, opTop = -1;
+
 void push(int value) {
     values[++top] = value;
 }
@@ -21,22 +23,30 @@ char peekOperator() {
     return opTop == -1 ? '\0' : operators[opTop];
 }
 int precedence(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
-    return 0;
+    int priority=1;
+    if (op == '+' || op == '-') priority= 1;
+    if (op == '*' || op == '/') priority= 2;
+    return priority;
 }
 int performOperation(int a, int b, char op) {
     if (op == '/' && b == 0) {
         printf("Error: Division by zero.\n");
         exit(1);
     }
+    int result=0;
     switch (op) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return a / b;
+        case '+': result= a + b;
+        break;
+        case '-': result= a - b;
+        break;
+        case '*': result= a * b;
+        break;
+        case '/': result= a / b;
+        break;
+        default: printf("Error: Invalid Operator.\n");
+                exit(1);
     }
-    return 0;
+    return result;
 }
 int evaluateExpression(const char* expr) {
     for (int i = 0; expr[i] != '\0'; i++) {
@@ -54,10 +64,10 @@ int evaluateExpression(const char* expr) {
             pushOperator(expr[i]);
         } else if (expr[i] == ')') {
             while (peekOperator() != '(') {
-                int b = pop();
-                int a = pop();
+                int operand1 = pop();
+                int operand2 = pop();
                 char op = popOperator();
-                push(performOperation(a, b, op));
+                push(performOperation(operand1, operand2, op));
             }
             popOperator();
         } else if (expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/') {
@@ -83,7 +93,8 @@ int evaluateExpression(const char* expr) {
     return pop();
 }
 int main() {
-    char expression[100];
+    
+    char expression[MAX];
 
     printf("Enter a mathematical expression: ");
     fgets(expression, 100, stdin);
